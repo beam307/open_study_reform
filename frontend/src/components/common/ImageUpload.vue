db0e2740d73949a1839be192f3ddc29ffef1bf2d
KimChanHee <beam307@naver.com>
2019-06-09 17:36:20 +0900

    파일업로드

---
+++ frontend/src/components/common/ImageUpload.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <file-pond
+      name="image_upload"
+      ref="pond"
+      label-idle="드래그 또는 클릭하세요.(최대 4개)"
+      allow-multiple="true"
+      maxFiles="4"
+      accepted-file-types="image/jpeg, image/png"
+      v-bind:files="thumbnail"
+      v-on:init="handleFilePondInit"/>
+  </div>
+</template>
+
+<script>
+  import vueFilePond from 'vue-filepond';
+  import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+  import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+
+  const FilePond = vueFilePond(FilePondPluginFileValidateType, FilePondPluginImagePreview);
+
+  export default {
+    data: function () {
+      return {
+        thumbnail: [],
+      };
+    },
+    methods: {
+      handleFilePondInit: function () {
+        console.log('FilePond has initialized');
+      }
+    },
+    components: {
+      FilePond
+    }
+  };
+</script>
+
+<style>
+  .image-upload .filepond--panel-root {
+    border: 2px solid #424242;
+  }
+
+  .image-upload .filepond--item {
+    width: calc(25% - .5em);
+  }
+
+  @media (max-width: 600px) {
+    .image-upload .filepond--item {
+      width: calc(100% - .5em);
+    }
+  }
+</style>
--- frontend/src/pages/study/register/studyRegisterStep3.vue
+++ frontend/src/pages/study/register/studyRegisterStep3.vue
@@ -8,20 +8,30 @@
         <v-textarea
           outline
           rows="10"
+          no-resize
         ></v-textarea>
+      </v-flex>
+    </v-layout>
+    <v-layout wrap>
+      <v-flex xs4 sm2 px-2 py-1>
+        <v-subheader style="align-items:flex-end">이미지</v-subheader>
+      </v-flex>
+      <v-flex xs8 sm10 px-2 py-1 class="image-upload">
+        <image-upload></image-upload>
       </v-flex>
     </v-layout>
   </v-card>
 </template>
 
 <script>
+  import imageUpload from '../../../components/common/ImageUpload';
 
   export default {
     data() {
-      return {
-      }
+      return {}
     },
     components: {
+      imageUpload
     }
   }
 </script>
