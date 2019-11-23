bcdea727e2837093f617c5266ed9e38472a26453
KimChanHee <beam307@naver.com>
2019-11-23 14:48:13 +0900

    properties -> yaml로 변경

--- src/main/java/org/project/study/StudyApplication.java
+++ src/main/java/org/project/study/StudyApplication.java
@@ -2,10 +2,10 @@ package org.project.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@PropertySource(value = { "classpath:${db.config}","classpath:${security.config}"  })
+@EnableConfigurationProperties
 public class StudyApplication {
 
     public static void main(String[] args) {
