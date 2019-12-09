4c300d2e2e4c0bc5eab52adb9469ae2e16e73f21
kimchanhee <beam307@naver.com>
2019-12-09 23:05:32 +0900

    vue static처리

--- src/main/java/org/project/study/controller/HomeController.java
+++ src/main/java/org/project/study/controller/HomeController.java
@@ -2,6 +2,7 @@ package org.project.study.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
@@ -10,5 +11,10 @@ public class HomeController {
     @GetMapping("/")
     public String index() {
         return "index.html";
+    }
+
+    @GetMapping("/**/{path:[^\\.]*}")
+    public String redirect() {
+        return "forward:/";
     }
 }
